695ead50feffc30e03708386b6cd59b0e4b08c37
BurbanoValentina <valentina.burbanos@campusucc.edu.co>
2025-11-13 12:59:37 -0500

    Add Icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CheckIcon: React.FC = () => (
+  <span aria-hidden="true">OK</span>
+);
+
+export const BackIcon: React.FC = () => (
+  <span aria-hidden="true">{"<-"}</span>
+);
