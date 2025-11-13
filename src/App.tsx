0aac94881b7eb84867600890fa79a5ae1efd371b
BurbanoValentina <valentina.burbanos@campusucc.edu.co>
2025-11-13 12:56:05 -0500

    Add App component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GameScreen from "./components/GameScreen";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <GameScreen />
+    </div>
+  );
+};
+
+export default App;
